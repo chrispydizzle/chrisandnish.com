6b704aeb1fb7f393d9a050f4302028c504a2e982
chris posada <CHRIS@cpsharp.net>
2017-07-17 11:18:57 -0400

    small patch for loader.

--- js/application.js
+++ js/application.js
@@ -2,7 +2,21 @@ let head;
 let pages;
 let header;
 let eleph;
-let isHover = false;
+
+(function (i, s, o, g, r, a, m) {
+    i['GoogleAnalyticsObject'] = r;
+    i[r] = i[r] || function () {
+            (i[r].q = i[r].q || []).push(arguments)
+        }, i[r].l = 1 * new Date();
+    a = s.createElement(o),
+        m = s.getElementsByTagName(o)[0];
+    a.async = 1;
+    a.src = g;
+    m.parentNode.insertBefore(a, m)
+})(window, document, 'script', 'https://www.google-analytics.com/analytics.js', 'ga');
+
+ga('create', 'UA-6178016-14', 'auto');
+ga('send', 'pageview');
 
 $(document).ready(function () {
     let winHeight = $(window).height();
@@ -32,6 +46,7 @@ $(document).ready(function () {
 
     eleph = $('.burger').children('span');
     fireRand(2000);
+    setTimeout(pingit, 1000);
 });
 
 function fireRand(time) {
